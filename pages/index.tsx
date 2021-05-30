5ac4cd222b5fa014ac5048b25aed38b03b6b8542
liram <liram07@gmail.com>
2021-05-31 02:23:15 +0300

    access metamask wallet and accounts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,53 @@
 import Link from 'next/link'
+import React, { useState } from 'react'
+import Web3 from 'web3'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const [accounts, setAccounts] = useState<string[]>([])
+  const getAccessToWallet = async () => {
+    const ethereum = window && (window as any).ethereum
+
+    if (ethereum) {
+      try {
+        await ethereum.enable()
+        console.log(true)
+      } catch (e) {
+        console.log(false)
+      }
+    }
+  }
+
+  const getAccounts = async () => {
+    const ethereum = window && (window as any).ethereum
+
+    if (ethereum) {
+      const web3 = new Web3(ethereum)
+
+      try {
+        const accounts = await web3.eth.getAccounts();
+        console.log('accounts', accounts)
+        setAccounts(accounts)
+      } catch (e) {
+        console.log('failed to access accounts')
+      }
+    }
+  }
+
+
+  return (
+    <Layout title='Home | Next.js + TypeScript Example'>
+      <h1>Hello Next.js 👋</h1>
+      <button onClick={getAccessToWallet}>add wallet</button>
+      <button onClick={getAccounts}>access accounts</button>
+      <p>
+        <Link href='/about'>
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  )
+}
 
 export default IndexPage
